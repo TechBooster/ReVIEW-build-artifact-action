0d533db81cd935d50470ee0204ef674cb2d47da8
mhidaka <rabbitlog@gmail.com>
2020-05-10 15:00:21 +0900

    - Fixed missing npm install commands.  - If an error occurs, make it fault and exit console.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/sh -el
 
 [ -z "${INPUT_TARGET_DIR}" ] && { echo "Need to set TARGET_DIR. Default is './'"; exit 1; }
 [ -z "${INPUT_CONFIG_FILE}" ] && { echo "Need to set CONFIG_FILE. Default is 'config.yml'"; exit 1; }
@@ -7,5 +7,5 @@
 # TODO Enable env paramaters that only INPUT_TARGET_DIR works well.
 
 echo "Run Initialization and build step"
-cd $INPUT_TARGET_DIR && bundle install && npm run pdf
+cd $INPUT_TARGET_DIR && bundle install && npm install && npm run pdf
 echo "Finish build step"
