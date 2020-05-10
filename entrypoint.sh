eeae63063d5e06080081aef6d3927882c1aabae7
mhidaka <rabbitlog@gmail.com>
2020-05-10 14:20:08 +0900

    initial commit of entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -l
+
+[ -z "${INPUT_TARGET_DIR}" ] && { echo "Need to set TARGET_DIR. Default is './'"; exit 1; }
+[ -z "${INPUT_CONFIG_FILE}" ] && { echo "Need to set CONFIG_FILE. Default is 'config.yml'"; exit 1; }
+[ -z "${INPUT_FORMAT}" ] && { echo "Need to set INPUT_FORMAT. Default is 'PDF'"; exit 1; }
+
+# TODO Enable env paramaters that only INPUT_TARGET_DIR works well.
+
+echo "Run Initialization and build step"
+cd $INPUT_TARGET_DIR && bundle install && npm run pdf
+echo "Finish build step"
