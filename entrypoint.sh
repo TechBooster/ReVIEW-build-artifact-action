f9c291ecf235dd18635623e9459eb2d0e9babfcb
Kazumi IWANAGA(OHIRA) / @dz_ <dzeyelid@gmail.com>
2023-10-20 02:04:41 +0000

    Add setting REVIEW_CONFIG_FILE

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,11 @@
 [ -z "${INPUT_CONFIG_FILE}" ] && { echo "Need to set CONFIG_FILE. Default is 'config.yml'"; exit 1; }
 [ -z "${INPUT_FORMAT}" ] && { echo "Need to set INPUT_FORMAT. Default is 'PDF'"; exit 1; }
 
-# TODO Enable env paramaters that only INPUT_TARGET_DIR works well.
+if [ -n "${INPUT_CONFIG_FILE}" ]; then
+  export REVIEW_CONFIG_FILE=$INPUT_CONFIG_FILE
+fi
+
+# TODO Enable INPUT_FORMAT env paramater
 
 echo "Run Initialization and build step"
 cd $INPUT_TARGET_DIR && bundle install && npm install && npm run pdf
